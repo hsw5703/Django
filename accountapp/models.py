44aa76b0a909d0b93246096d99d1539c8039b985
hsw5703 <hsw5703@naver.com>
2021-07-12 12:05:48 +0900

    multi_str_display

--- accountapp/models.py
+++ accountapp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class HelloWorld(models.Model):
+class Helloworld(models.Model):
     text = models.CharField(max_length=255, null=False)
 
 # model 생성 명령어 : python manange.py makemigrations
--- accountapp/views.py
+++ accountapp/views.py
@@ -2,14 +2,24 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from accountapp.models import Helloworld
+
 
 def hello_world(request):
     if request.method == 'POST':
 
         temp = request.POST.get('input_text')
         # input_txt라는 이름을 가진 hello_world.html의 값을 가져온다.
+
+        new_hello_world = Helloworld()
+        new_hello_world.text = temp
+        new_hello_world.save()
+
+        hello_world_list = Helloworld.objects.all()
+
         return render(request, 'accountapp\hello_world.html',
-                      context={'text': temp})
+                      context={'hello_world_list': hello_world_list})
     else:
+        hello_world_list = Helloworld.objects.all()
         return render(request, 'accountapp\hello_world.html',
-                      context={'text':'GET METHOD!'})
+                      context={'hello_world_list': hello_world_list})
