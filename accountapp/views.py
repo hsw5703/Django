ab8973a9dcbb90bcb56995adca9bf5162122893f
hsw5703 <hsw5703@naver.com>
2021-07-12 11:12:03 +0900

    input_text + text_display

--- accountapp/views.py
+++ accountapp/views.py
@@ -5,8 +5,11 @@ from django.shortcuts import render
 
 def hello_world(request):
     if request.method == 'POST':
+
+        temp = request.POST.get('input_text')
+        # input_txt라는 이름을 가진 hello_world.html의 값을 가져온다.
         return render(request, 'accountapp\hello_world.html',
-                      context={'text':'POST METHOD!'})
+                      context={'text': temp})
     else:
         return render(request, 'accountapp\hello_world.html',
                       context={'text':'GET METHOD!'})
