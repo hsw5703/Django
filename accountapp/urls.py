5b01baf783054c0f840f69d3edd4a61a5fc8b26c
hsw5703 <hsw5703@naver.com>
2021-07-19 12:24:09 +0900

    plus delete function

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from accountapp.views import hello_world, AccountCreateView, AccountDetailView, AccountUpdateView
+from accountapp.views import hello_world, AccountCreateView, AccountDetailView, AccountUpdateView, AccountDeleteView
 
 app_name = 'accountapp'
 
@@ -10,6 +10,7 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='accountapp/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('create/', AccountCreateView.as_view(), name='create'),
-    path('detail/<int:pk>', AccountDetailView.as_view(), name='detail'), # pk라고 숫자를 받음.
+    path('detail/<int:pk>', AccountDetailView.as_view(), name='detail'),  # pk(primary key) : detail, update, delete는 중복되면 X
     path('update/<int:pk>', AccountUpdateView.as_view(), name='update'),
+    path('delete/<int:pk>', AccountDeleteView.as_view(), name='delete'),
 ]
