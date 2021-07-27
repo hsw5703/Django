6a5770f05dceb15b0cf3e95950ed77880e53f325
hsw5703 <hsw5703@naver.com>
2021-07-27 17:13:47 +0900

    7/27 - profile upload complete

--- profileapp/views.py
+++ profileapp/views.py
@@ -13,6 +13,6 @@ class ProfileCreateView(CreateView):
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/create.html'
 
-    def form_valid(self, form):
+    def form_valid(self, form):  # 파일의 형식이 이미지인지 판별 후 이미지가 맞다면 이 구문을 실행한다.
         form.instance.user = self.request.user
         return super().form_valid(form)
