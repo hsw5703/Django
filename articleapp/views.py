6ae8fe46aaccbea9883229baccc81956f27fe090
hsw5703 <hsw5703@naver.com>
2021-08-03 17:39:31 +0900

    8/3 - ArticleDetailView

--- articleapp/views.py
+++ articleapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 
 from articleapp.forms import ArticleCreationForm
 from articleapp.models import Article
@@ -15,3 +15,8 @@ class ArticleCreateView(CreateView):
     def form_valid(self, form):
         form.instance.writer = self.request.user
         return super().form_valid(form)
+
+class ArticleDetailView(DetailView):
+    model = Article
+    context_object_name = 'target_article'
+    template_name = 'articleapp/detail.html'
