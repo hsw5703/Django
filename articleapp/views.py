2f7fe58d7b796a2800d49c200f05b6fbc3be7e40
hsw5703 <hsw5703@naver.com>
2021-08-03 16:54:02 +0900

    8/3 - ArticleCreateView

--- articleapp/views.py
+++ articleapp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+from articleapp.forms import ArticleCreationForm
+from articleapp.models import Article
+
+
+class ArticleCreateView(CreateView):
+    model = Article
+    form_class = ArticleCreationForm
+    success_url = reverse_lazy('articleapp:list')
+    template_name = 'articleapp/create.html'
+
+    def form_valid(self, form):
+        form.instance.writer = self.request.user
+        return super().form_valid(form)
