f9c0cd9563a1af60edd89c979bc35cc29992fc3e
hsw5703 <hsw5703@naver.com>
2021-08-09 11:27:19 +0900

    8/9 - commentapp's models, forms, migrations

--- commentapp/models.py
+++ commentapp/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+from articleapp.models import Article
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.SET_NULL,
+                                related_name='comment', null=True)  # on_delete 구문에서 연결돼 있는 Article이 삭제되면 comment는 일단 남아있게 설정
+    writer = models.ForeignKey(User, on_delete=models.SET_NULL,
+                               related_name='comment', null=True)
+    content = models.TextField(null=False)  # 장문의 경우 TextField, 단문의 경우 CharField 사용
+    created_at = models.DateTimeField(auto_now_add=True)
